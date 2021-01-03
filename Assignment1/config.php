50b20df611fd3793a22553166b8e6ac9d4b9e4a1
Jibank21 <68716761+Jibank21@users.noreply.github.com>
2021-01-03 18:27:03 +1100

    Add files via upload

---
+++ Assignment1/config.php
@@ -0,0 +1,9 @@
+<?php
+$serverName = "localhost";
+$username = "root";
+$Password ="";
+$database = "itech3108_30370214_a1.";
+//to connect with server
+$conn = mysqli_connect($serverName,$username,$Password,$database);
+
+?>
